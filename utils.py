f76856b041b7536af2949b2dd33f0d7c9cea55f8
John You <johnswyou@gmail.com>
2023-03-15 19:32:24 -0400

    Add add_lagged_variables and dependent funcs

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import pandas as pd
 
 def load_pickle(filepath):
     with open(filepath, 'rb') as f:
@@ -24,3 +25,56 @@ def circ_conv(signal, ker):
         signal and ker must have same shape
     '''
     return np.real(np.fft.ifft( np.fft.fft(signal)*np.fft.fft(ker)))
+
+def add_lags(x, n_lags, pandas_output = False):
+
+    x = pd.Series(x)
+    
+    cols = [x]
+    
+    for i in range(1, n_lags+1):
+        cols.append(x.shift(i))
+
+    if pandas_output:
+        return pd.concat(cols, axis=1).dropna()
+    else:
+        return pd.concat(cols, axis=1).dropna().to_numpy()
+
+def make_lag_names(n_inputs, n_lags):
+    orig_input_names = ["X" + str(i) for i in range(1, n_inputs+1)]
+    lag_names = ["lag_" + str(i) for i in range(n_lags+1)]
+    out = [i + "_" + j for i in orig_input_names for j in lag_names]
+    out = [i.replace('_lag_0', '') for i in out]
+    return out
+
+def add_lagged_variables(X, y=None, n_lags=1, pandas_output=False):
+
+    '''
+    X is a 2D numpy array
+    **kwargs are further arguments to be passed to modwt()
+    '''
+    
+    assert len(X.shape) > 1
+    assert X.shape[1] > 0
+    assert X.shape[0] > X.shape[1]
+
+    n_inputs = X.shape[1]
+
+    out = np.apply_along_axis(add_lags, 0, X, n_lags, False)
+    out = np.split(out, n_inputs, 2)
+    out = [i.squeeze() for i in out]
+    out = np.concatenate(out, axis=1)
+
+    if pandas_output:
+        out = pd.DataFrame(out, columns=make_lag_names(n_inputs, n_lags))
+
+    if y is not None:
+        assert isinstance(y, np.ndarray)
+        assert len(y.shape) == 1
+        y = y[n_lags:]
+        assert out.shape[0] == y.shape[0]
+        if pandas_output:
+            y = pd.Series(y, name="y")
+        return out, y
+    else:
+        return out
